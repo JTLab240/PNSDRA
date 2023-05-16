884877d9338d0b71a2b63d415dbd19b0ab7efdcd
JTLab240 <133069885+JTLab240@users.noreply.github.com>
2023-05-15 18:53:31 -0700

    Add files via upload

--- main_preproc.m
+++ main_preproc.m
@@ -88,25 +88,47 @@ sFiles = bst_process('CallProcess', 'process_import_data_time', sFiles, [], ...
     'baseline',      [], ...
     'blsensortypes', 'MEG, EEG');
 
+% Process: Hilbert transform
+sFiles2 = bst_process('CallProcess', 'process_hilbert', sFiles, [], ...
+    'sensortypes',   'MEG, EEG', ...
+    'edit',          struct(...
+         'Comment',         'Power', ...
+         'TimeBands',       [], ...
+         'Freqs',           {{'delta', '0.5, 4', 'mean'; 'theta', '5, 7', 'mean'; 'alpha', '8, 12', 'mean'; 'beta', '15, 29', 'mean'; 'gamma1', '30, 59', 'mean'; 'gamma2', '60, 90', 'mean'}}, ... % Specify frequency band ranges
+         'ClusterFuncTime', 'none', ...
+         'Measure',         'power', ...
+         'Output',          'all', ...
+         'SaveKernel',      0), ...
+    'normalize2020', 0, ...
+    'normalize',     'none', ...  % None: Save non-standardized time-frequency maps
+    'mirror',        0);
+
 %% Saving data
 
-bst_db_dir = 'C:\Users\Jay\Desktop\Data\BrainStorm\brainstorm_db\';
+bst_db_dir = 'C:\Users\Jay\Desktop\Data\BrainStorm\brainstorm_db\'; % File directory
 
-Result.data = []; Result.Time = [];
+Result.EEG = []; Result.Time = []; Result.Power = []; Result.Frequency = [];
 
 for i = 1:size(sFiles,2)    
+    
+    file = [bst_db_dir, '\',ProtocolName,'\',sFiles(i).FileType,'\', sFiles(i).FileName];   % Preprocessed EEG file name
+    tmp_eeg = load(file);
+    
+    file2 = [bst_db_dir, '\',ProtocolName,'\',sFiles(i).FileType,'\', sFiles2(i).FileName]; % Average power (Hilbert transform) file name
+    tmp_power = load(file2);
+    
     if i == 1
         fileChan = [bst_db_dir, '\',ProtocolName,'\',sFiles(i).FileType,'\', sFiles(i).ChannelFile];
         load(fileChan)
-        Result.channel = Channel;
+        Result.channel = Channel;   % Channel information
+        
+        Result.Frequency = tmp_power.Freqs; % Frequency band information
     else
     end
-            
-    file = [bst_db_dir, '\',ProtocolName,'\',sFiles(i).FileType,'\', sFiles(i).FileName];
-    load(file);
-    
-    Result.data = cat(3, Result.data, F);
-    Result.Time = [Result.Time; Time];
+
+    Result.EEG = cat(3, Result.EEG, F); % channel x time x epoch
+    Result.Time = [Result.Time; Time];  % epoch x time
+    Result.Power = cat(4, Result.Power, tmp_power.TF);  % channel x time x frequency x epoch
 end
 
 save([path,name,'_preprocessed.mat'],'Result','-v7.3');
